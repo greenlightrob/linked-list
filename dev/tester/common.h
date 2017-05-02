4337c2c7b4fac1353fdb5b215abaefa5dbdf7886
davidkristoffersen <david.alta2010@gmail.com>
2017-05-02 03:17:17 +0200

    Added common header

--- dev/tester/common.h
+++ dev/tester/common.h
@@ -10,10 +10,14 @@ void fatal_error(char *msg, ...);
 // Generic compare functions
 typedef int (*cmpfunc_t)(void *, void *);
 typedef void *(*copyfunc_t)(void *);
+typedef void (*destroy_t)(void *);
+
+void destroy_int(void *input);
 
 // Type specific compare functions
 int compare_int(void *a, void *b);
 int compare_string(void *a, void *b);
 int compare_pointer(void *a, void *b);
 
+int *allocate_int(int input);
 #endif
